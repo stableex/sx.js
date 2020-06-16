c47757df772236553bb0c2f2319aa6a63183140e
Denis <carriere.denis@gmail.com>
2020-06-16 19:10:51 -0400

    update eos-common

--- examples/get_rate.ts
+++ examples/get_rate.ts
@@ -1,5 +1,6 @@
 import { rpc } from "./config";
-import { get_tokens, get_settings, get_rate } from "..";
+import { get_tokens, get_settings, get_rate, get_slippage } from "..";
+import { asset } from "eos-common";
 
 (async () => {
     // settings
@@ -7,12 +8,20 @@ import { get_tokens, get_settings, get_rate } from "..";
     const tokens = await get_tokens( rpc, code );
     const settings = await get_settings( rpc, code );
 
+    tokens["EOSDT"].balance = asset("5000.000000000 EOSDT");
+    tokens["EOSDT"].depth = asset("5000.000000000 EOSDT");
+    tokens["EOS"].balance = asset("2000.0000 EOS");
+    tokens["EOS"].depth = asset("2000.0000 EOS");
+    settings.fee = 50;
+
     // calculate rate
-    const quantity = "100.0000 EOS";
+    const quantity = "500.0000 EOS";
     const symcode = "EOSDT";
     const rate = get_rate( quantity, symcode, tokens, settings );
+    const slippage = get_slippage( quantity, symcode, tokens, settings );
 
     // logs
     console.log("quantity:", quantity );
     console.log("rate:", rate.to_string());
+    console.log("slippage:", slippage );
 })();
