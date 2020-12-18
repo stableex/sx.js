b03402c6c2c4e65e6ef94b5b77b26e30c5af9545
Denis <carriere.denis@gmail.com>
2020-12-17 22:56:30 -0500

    update growth to hourly

--- src/get_vault_growth.ts
+++ src/get_vault_growth.ts
@@ -3,16 +3,17 @@ import { Asset } from "eos-common";
 import { get_vault } from "./get_vault";
 
 export interface Growth {
+    delta_block_num: number,
     previous: number,
     current: number,
     delta: number,
 }
 
 export async function get_vault_apy( growth: Growth ) {
-    return growth.delta * 365 / growth.current;
+    return growth.delta * ( 172800 / growth.delta_block_num  ) * 365 / growth.current;
 }
 
-export async function get_vault_growth( client: DfuseClient, symcode: string, last_irreversible_block_num: number, delta_block_num = 172800 ): Promise<Growth> {
+export async function get_vault_growth( client: DfuseClient, symcode: string, last_irreversible_block_num: number, delta_block_num = 7200, ): Promise<Growth> {
     const previous_vault = await get_vault( client, symcode, last_irreversible_block_num - delta_block_num );
     const current_vault = await get_vault( client, symcode, last_irreversible_block_num );
 
@@ -26,6 +27,7 @@ export async function get_vault_growth( client: DfuseClient, symcode: string, la
     const current = current_supply / current_deposit;
 
     return {
+        delta_block_num,
         previous,
         current,
         delta: previous - current
