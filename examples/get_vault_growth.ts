666db5416e62a0f89cb571de74ee385f7c511511
Denis <carriere.denis@gmail.com>
2020-12-28 22:52:12 -0500

    update example

--- examples/get_vault_growth.ts
+++ examples/get_vault_growth.ts
@@ -8,16 +8,18 @@ import { get_vault_growth, get_vault_apy } from "../src/get_vault_growth";
     const growth = await get_vault_growth(client, "EOS", last_irreversible_block_num - 1200 );
     console.log(growth);
     // {
+    //     previous_block_num: 159955872,
+    //     current_block_num: 160128672,
     //     delta_block_num: 172800,
-    //     previous: 9538.13194729921,
-    //     current: 9535.720685970111,
-    //     delta: 2.4112613290999434
+    //     previous: 9535.652695255621,
+    //     current: 9533.441474760219,
+    //     delta: 2.211220495402813
     // }
 
     // APY
     const apy = await get_vault_apy( growth );
     console.log(apy);
-    //=> 0.09229615821448967
+    //=> 0.08465940478669866
 
     client.release()
 })();
