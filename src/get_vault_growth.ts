81a9bef054af5285233495632f884236af83a426
Denis <carriere.denis@gmail.com>
2020-12-28 22:51:17 -0500

    add extra details in vault growth

--- src/get_vault_growth.ts
+++ src/get_vault_growth.ts
@@ -4,6 +4,8 @@ import { Vault, get_vault_rate } from "./get_vault";
 import { stateTableRow } from "./dfuse";
 
 export interface Growth {
+    previous_block_num: number,
+    current_block_num: number,
     delta_block_num: number,
     previous: number,
     current: number,
@@ -19,14 +21,18 @@ export async function get_vault_apy( growth: Growth ) {
 }
 
 export async function get_vault_growth( client: DfuseClient, symcode: string, last_irreversible_block_num: number, delta_block_num = 172800 ): Promise<Growth> {
-    const previous_vault = await get_dfuse_vault( client, symcode, last_irreversible_block_num - delta_block_num );
-    const current_vault = await get_dfuse_vault( client, symcode, last_irreversible_block_num );
+    const previous_block_num = last_irreversible_block_num - delta_block_num;
+    const current_block_num = last_irreversible_block_num;
+    const previous_vault = await get_dfuse_vault( client, symcode, previous_block_num );
+    const current_vault = await get_dfuse_vault( client, symcode, current_block_num );
 
     // value per share
     const previous = get_vault_rate( previous_vault );
     const current = get_vault_rate( current_vault );
 
     return {
+        previous_block_num,
+        current_block_num,
         delta_block_num,
         previous,
         current,
