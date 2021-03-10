22fa47ad71ea0a199b3fd30594fee8c3cf4c624e
Denis Carriere <carriere.denis@gmail.com>
2021-03-10 10:40:11 -0500

    update

--- examples/get_flash_growth.ts
+++ examples/get_flash_growth.ts
@@ -9,21 +9,28 @@ import { get_flash_growth } from "../src/sx.flash";
         const growth = await get_flash_growth(client, symcode, last_irreversible_block_num );
         console.log(growth);
         // {
+        //     block_num: 172474693,
+        //     block_num_previous: 172301893,
+        //     block_num_delta: 172800,
         //     contract: 'flash.sx',
-        //     last_modified: '2021-03-10T15:36:59',
-        //     transactions: 424920,
+        //     last_modified: '2021-03-10T15:36:23',
+        //     transactions: 424918,
         //     borrow: [
-        //       { key: 'EOS', value: '10293286.2997 EOS' },
-        //       { key: 'USDT', value: '740167.8216 USDT' }
+        //       { key: 'EOS', value: '10293285.2997 EOS' },
+        //       { key: 'USDT', value: '740109.6146 USDT' }
         //     ],
         //     fees: [
-        //       { key: 'EOS', value: '2023.7615 EOS' },
-        //       { key: 'USDT', value: '147.3830 USDT' }
+        //       { key: 'EOS', value: '2023.7613 EOS' },
+        //       { key: 'USDT', value: '147.3714 USDT' }
         //     ],
         //     reserves: [
-        //       { key: 'EOS', value: '790572.4783 EOS' },
-        //       { key: 'USDT', value: '282311.0727 USDT' }
-        //     ]
+        //       { key: 'EOS', value: '790572.4781 EOS' },
+        //       { key: 'USDT', value: '282311.0611 USDT' }
+        //     ],
+        //     borrow_delta: 1300812.3515999988,
+        //     fees_delta: 259.7091999999998,
+        //     reserves_delta: 86767.05039999995,
+        //     transactions_delta: 15047
         // }
     }
 
